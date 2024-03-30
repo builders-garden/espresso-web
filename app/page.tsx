0d5691fd8b8a0229b0ca8dc66bd5b4de4772226d
lim0n3 <simonestaffa96@gmail.com>
2024-03-30 04:14:49 +0200

    finalize landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,40 @@
 "use client";
 
+import { Link } from "@nextui-org/react";
 import Portal from "../components/graphics/portal";
 
 export default function Home() {
   return (
     <>
       <main className="flex min-h-screen min-w-full">
-        <div className="flex bg-privy-light-blue flex-1 p-6 justify-center items-center">
-          <div>
-            <div>
-              <Portal style={{ maxWidth: "100%", height: "auto" }} />
+        <div className="flex flex-col flex-1 p-6 justify-center items-center space-y-8">
+          <div className="flex flex-col flex-1 p-6 justify-center items-center space-y-4">
+            <div className="text-6xl text-center font-black">Espresso ☕️</div>
+            <div className="text-2xl text-center font-semibold">
+              Selling goods in crypto has never been easier.
             </div>
-            <div className="mt-6 flex justify-center text-center">
-              <button className="bg-violet-600 hover:bg-violet-700 py-3 px-6 text-white rounded-lg">
-                Log in
-              </button>
+          </div>
+          <div className="flex flex-col flex-1 p-6 justify-center items-center space-y-4">
+            <div className="flex flex-row justify-between space-x-4">
+              <Link href="https://www.youtube.com/watch?v=Rg5apXr-K50">
+                Watch our demo 🎥
+              </Link>
+            </div>
+            <div className="flex flex-row space-x-2">
+              <p>Github:</p>
+              <Link href="https://github.com/builders-garden/espresso-web">
+                Webapp
+              </Link>
+              <Link href="https://github.com/builders-garden/espresso-app">
+                App
+              </Link>
+              <Link href="https://github.com/builders-garden/espresso-smart-contracts">
+                Smart Contracts
+              </Link>
+            </div>
+            <div className="flex flex-row space-x-2">
+              <p>Made with ❤️ by</p>
+              <Link href="https://builders.garden">builders garden</Link>
             </div>
           </div>
         </div>
